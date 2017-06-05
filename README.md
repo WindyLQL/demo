a7a57820c31866d2e439d7e8a6f56873fc4d32a2
root <root@localhost.localdomain>
2017-06-05 16:58:15 +0800

    ibevent demo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # demo
+# shared_memory
+
+  gcc shread.c -o read
+  gcc shwrite.c -o write
+  
+# libeventdemo
+  client3.c server3.c最高级写法
+  client1.c server1.c低级写法
+  g++ client.cpp -o client -levent
+  gcc client1.c -o client -levent
+  g++ server.cpp -o server -levent
+  gcc server.c -o server -levent
---
+++ libeventdemo/server.cpp
@@ -0,0 +1,106 @@
+#include <stdio.h>  
+#include <unistd.h>
+#include <string.h>  
+#include <iostream>  
+#include <sys/socket.h>      
+#include <netinet/in.h>      
+#include <arpa/inet.h>      
+#include <netdb.h>  
+  
+#include <event.h>  
+using namespace std;  
+  
+// 事件base  
+struct event_base* base;  
+  
+// 读事件回调函数  
+void onRead(int iCliFd, short iEvent, void *arg)  
+{  
+    int iLen;  
+    char buf[1500];  
+  
+    iLen = recv(iCliFd, buf, 1500, 0);  
+  
+    if (iLen <= 0) {  
+        cout << "Client Close" << endl;  
+  
+        // 连接结束(=0)或连接错误(<0)，将事件删除并释放内存空间  
+        struct event *pEvRead = (struct event*)arg;  
+        event_del(pEvRead);  
+        delete pEvRead;  
+  
+        close(iCliFd);  
+        return;  
+    }  
+  
+    buf[iLen] = 0;  
+    cout << "Client Info:" << buf << endl;  
+
+
+
+	struct bufferevent* buf_ev;
+	buf_ev = bufferevent_new(iCliFd, NULL, NULL, NULL, NULL);
+	buf_ev->wm_read.high = 4096;
+	char MESSAGE[]="welcome to server..";
+	bufferevent_write(buf_ev, MESSAGE, strlen(MESSAGE));
+}  
+  
+// 连接请求事件回调函数  
+void onAccept(int iSvrFd, short iEvent, void *arg)  
+{  
+    int iCliFd;  
+    struct sockaddr_in sCliAddr;  
+  
+    socklen_t iSinSize = sizeof(sCliAddr);  
+    iCliFd = accept(iSvrFd, (struct sockaddr*)&sCliAddr, &iSinSize);  
+  
+    // 连接注册为新事件 (EV_PERSIST为事件触发后不默认删除)  
+    struct event *pEvRead = new event;  
+    event_set(pEvRead, iCliFd, EV_READ|EV_PERSIST, onRead, pEvRead);  
+    event_base_set(base, pEvRead);  
+    event_add(pEvRead, NULL); 
+
+
+	struct bufferevent* buf_ev;  
+	 buf_ev = bufferevent_new(iCliFd, NULL, NULL, NULL, NULL);    
+     buf_ev->wm_read.high = 4096;
+	 char MESSAGE[]="welcome to server..";
+	bufferevent_write(buf_ev, MESSAGE, strlen(MESSAGE)); 
+
+	cout<<"a client connect:"<<iCliFd<<endl;
+}  
+  
+int main()  
+{  
+  
+    int iSvrFd;    
+    struct sockaddr_in sSvrAddr;  
+                
+    memset(&sSvrAddr, 0, sizeof(sSvrAddr));    
+    sSvrAddr.sin_family = AF_INET;    
+    sSvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");      
+    sSvrAddr.sin_port = htons(8888);     
+                              
+    // 创建tcpSocket（iSvrFd），监听本机8888端口    
+    iSvrFd = socket(AF_INET, SOCK_STREAM, 0);    
+    bind(iSvrFd, (struct sockaddr*)&sSvrAddr, sizeof(sSvrAddr));    
+    listen(iSvrFd, 10);  
+  
+    // 初始化base  
+    base = (struct event_base*)event_init();  
+      
+    struct event evListen;  
+    // 设置事件  
+    event_set(&evListen, iSvrFd, EV_READ|EV_PERSIST, onAccept, NULL);  
+    // 设置为base事件  
+    event_base_set(base, &evListen);  
+    // 添加事件  
+    event_add(&evListen, NULL);  
+      
+    // 事件循环  
+    event_base_dispatch(base);  
+  
+    return 0;  
+}  
+
+
